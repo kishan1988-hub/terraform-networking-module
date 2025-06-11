d7848ba694eaf4414469933e3895c811a5f1d8d7
kishan1988-hub <kishan1988@gmail.com>
2025-06-11 22:08:57 +0530

    variable validation introduced

--- outputs.tf
+++ outputs.tf
@@ -5,10 +5,12 @@ output "vpc_id" {
 
 output "public_subnet_id" {
   description = "List of public subnet IDs"
-  value = values(aws_subnet.public[*]).id
+#   value = values(aws_subnet.public[*]).id
+value = [for s in values(aws_subnet.public) : s.id]
 }
 
 output "private_subnet_id" {
   description = "list of private subnet ID"
-  value = values(aws_subnet.private)[*].id
+#   value = values(aws_subnet.private)[*].id
+value = [for s in values(aws_subnet.private) : s.id]
 }
--- variables.tf
+++ variables.tf
@@ -2,15 +2,31 @@ variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type = string
   default = "10.0.0.0/16"
+  validation {
+    condition = can(regex("^([0-9]{1,3}\\.){3}[0-9]{1,3}/[0-9]+$",var.vpc_cidr))
+    error_message = "allowed CIDR must be valid CIDR"
+}
 }
 
 variable "public_subnet_cidr" {
   description = "List of CIDR for public subnets"
   type = list(string)
+  default = ["10.0.1.0/24","10.0.2.0/24"]
+
+  validation {
+    condition = length(var.public_subnet_cidr) > 0
+    error_message = "You must supply atleast one public CIDR"
+  }
 }
 
 variable "private_subnet_cidr" {
   description = "List of CIDR for private subnets"
   type = list(string)
+  default = ["10.0.3.0/24","10.0.4.0/24"]
+
+  validation {
+    condition = length(var.private_subnet_cidr) > 0
+    error_message = "You must supply atleast one private CIDR"
+  }
 }
 
